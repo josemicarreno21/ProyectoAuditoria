aabafd8e1a2d9dc4203b74c13d669e53390baf08
josemicarreno21 <josemicarreno21@gmail.com>
2022-05-25 12:07:17 -0400

    a2

--- ProyectoAuditoria/ProyectoAuditoria/Form1.cs
+++ ProyectoAuditoria/ProyectoAuditoria/Form1.cs
@@ -17,21 +17,55 @@ namespace ProyectoAuditoria
             InitializeComponent();
         }
 
-        private void txtRPO1_TextChanged(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            string detRPO, detRTO;
-            if(Int32.Parse(txtRPO1.Text)<0.25 && Int32.Parse(txtRPO1.Text) >= 0.01)
+            string detRPO = "", detRTO = "";
+            if (Double.Parse(txtRPO1.Text) < 0.25 && Double.Parse(txtRPO1.Text) >= 0.01)
             {
                 detRPO = "Alta disponibilidad";
             }
-            if (Int32.Parse(txtRPO1.Text) < 24 && Int32.Parse(txtRPO1.Text) >= 0.25)
+            if (Double.Parse(txtRPO1.Text) < 24 && Double.Parse(txtRPO1.Text) >= 0.25)
             {
                 detRPO = "Replicación asíncrona";
             }
-            if (Int32.Parse(txtRPO1.Text) >= 24)
+            if (Double.Parse(txtRPO1.Text) >= 24)
             {
                 detRPO = "Respaldo";
             }
+            if (Double.Parse(txtRTO1.Text) < 1 && Double.Parse(txtRTO1.Text) >= 0.01)
+            {
+                detRTO = "Hot Site";
+            }
+            if (Double.Parse(txtRTO1.Text) < 24 && Double.Parse(txtRTO1.Text) >= 2)
+            {
+                detRTO = "Sitio tibio";
+            }
+            if (Double.Parse(txtRTO1.Text) >= 24)
+            {
+                detRTO = "Sitio frio";
+            }
+            lblDetalle1.Text = detRPO + "/" + detRTO;
+
+
+            double LBC = Double.Parse(txtRPO1.Text) + Double.Parse(txtRTO1.Text);
+            lblLBC1.Text = LBC.ToString();
+
+
+            double prioridad = 0;
+            if (LBC < 1)
+            {
+                prioridad = 1;
+            }
+            if (LBC < 24 && LBC > 1)
+            {
+                prioridad = 2;
+            }
+            if (LBC >= 24)
+            {
+                prioridad = 3;
+            }
+
+            lblPrioridad1.Text = prioridad.ToString();
         }
     }
 }
