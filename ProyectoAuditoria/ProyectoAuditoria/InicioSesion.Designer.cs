597511e63262f9f095e3ac4d13d24693e76f63c2
josemicarreno21 <josemicarreno21@gmail.com>
2022-05-30 00:23:02 -0400

    cgvhb

--- ProyectoAuditoria/ProyectoAuditoria/InicioSesion.Designer.cs
+++ ProyectoAuditoria/ProyectoAuditoria/InicioSesion.Designer.cs
@@ -44,10 +44,9 @@ namespace ProyectoAuditoria
             this.btnIniciarSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
             this.btnIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIniciarSesion.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnIniciarSesion.Location = new System.Drawing.Point(319, 415);
-            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(4);
+            this.btnIniciarSesion.Location = new System.Drawing.Point(239, 337);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(272, 60);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(204, 49);
             this.btnIniciarSesion.TabIndex = 22;
             this.btnIniciarSesion.Text = "INICIAR SESION";
             this.btnIniciarSesion.UseVisualStyleBackColor = false;
@@ -56,20 +55,18 @@ namespace ProyectoAuditoria
             // txtContrasenia
             // 
             this.txtContrasenia.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContrasenia.Location = new System.Drawing.Point(416, 325);
-            this.txtContrasenia.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContrasenia.Location = new System.Drawing.Point(312, 264);
             this.txtContrasenia.Name = "txtContrasenia";
             this.txtContrasenia.PasswordChar = '*';
-            this.txtContrasenia.Size = new System.Drawing.Size(352, 45);
+            this.txtContrasenia.Size = new System.Drawing.Size(265, 37);
             this.txtContrasenia.TabIndex = 20;
             // 
             // txtUsuario
             // 
             this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuario.Location = new System.Drawing.Point(416, 257);
-            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUsuario.Location = new System.Drawing.Point(312, 209);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(352, 45);
+            this.txtUsuario.Size = new System.Drawing.Size(265, 37);
             this.txtUsuario.TabIndex = 19;
             // 
             // label3
@@ -77,10 +74,9 @@ namespace ProyectoAuditoria
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(178, 328);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(134, 266);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(191, 38);
+            this.label3.Size = new System.Drawing.Size(164, 32);
             this.label3.TabIndex = 15;
             this.label3.Text = "Contraseña:";
             // 
@@ -89,35 +85,36 @@ namespace ProyectoAuditoria
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(236, 260);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(177, 211);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 38);
+            this.label2.Size = new System.Drawing.Size(116, 32);
             this.label2.TabIndex = 14;
             this.label2.Text = "Usuario:";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(171, 30);
+            this.pictureBox1.Location = new System.Drawing.Point(128, 24);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(612, 204);
+            this.pictureBox1.Size = new System.Drawing.Size(459, 166);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 23;
             this.pictureBox1.TabStop = false;
             // 
             // InicioSesion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(105)))), ((int)(((byte)(176)))));
-            this.ClientSize = new System.Drawing.Size(939, 542);
+            this.ClientSize = new System.Drawing.Size(704, 409);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnIniciarSesion);
             this.Controls.Add(this.txtContrasenia);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "InicioSesion";
             this.Text = "InicioSesion";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
